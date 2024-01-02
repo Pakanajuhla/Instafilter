aa4d0965bdf6d0eb393334d239081e4812d601e5
Vladyslav <135268924+Pakanajuhla@users.noreply.github.com>
2024-01-02 15:16:47 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Владислав on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
